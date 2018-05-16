01804307bfd1e498d89f98a4896544be51ae53ad
lindongwu <lindongwu@memect.co>
2018-05-16 20:24:47 +0800

    feat: more example

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Flex from 'styled-flex-component'
 
 const Container = styled.article``;
 
 const Intro = styled.h1``;
 
-const Examples = styled.div``;
-const Example = styled.a``;
+const Examples = styled(Flex)``;
+const Example = styled.a`
+  margin-top: 20px;
+`;
 
 export default class Home extends Component {
   render() {
@@ -15,9 +18,15 @@ export default class Home extends Component {
         <Intro>
           try add <pre>/ipfs/somehashblabla</pre> to the URL
         </Intro>
-        <Examples>
+        <Examples column>
           <Example href="/ipfs/QmeYxwj4CwCeGVhwi3xLrmBZUUFQdftshSiGLrTdTnWEVV">
-            ipfs/QmeYxwj4CwCeGVhwi3xLrmBZUUFQdftshSiGLrTdTnWEVV
+          directory containing a web page: ipfs/QmeYxwj4CwCeGVhwi3xLrmBZUUFQdftshSiGLrTdTnWEVV
+          </Example>
+          <Example href="/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG">
+          directory listing: ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG
+          </Example>
+          <Example href="/ipfs/QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o">
+          simple file: ipfs/QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o
           </Example>
         </Examples>
       </Container>
