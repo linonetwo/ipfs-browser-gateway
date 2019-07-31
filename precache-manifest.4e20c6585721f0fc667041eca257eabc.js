cbbcdd862569ee0e149ec126e992c48ba5c194f2
林东吴 <lindongwu11@gmail.com>
2019-07-31 22:06:39 +0800

    Updates

---
+++ precache-manifest.4e20c6585721f0fc667041eca257eabc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e4fc860a5160deeccba55ffd508490",
+    "url": "/ipfs-browser-gateway/index.html"
+  },
+  {
+    "revision": "48510602d538b86da275",
+    "url": "/ipfs-browser-gateway/static/js/2.c909d0a5.chunk.js"
+  },
+  {
+    "revision": "2e286ba30c8fb475a679",
+    "url": "/ipfs-browser-gateway/static/js/main.0079e1a1.chunk.js"
+  },
+  {
+    "revision": "f7bc63606484eb11c4d3",
+    "url": "/ipfs-browser-gateway/static/js/runtime~main.9569e056.js"
+  },
+  {
+    "revision": "b0085fa66d616aed6a86467c22918eea",
+    "url": "/ipfs-browser-gateway/sw.js"
+  }
+]);
