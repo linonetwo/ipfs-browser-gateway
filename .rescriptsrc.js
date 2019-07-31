66ef365c2cf03e459098162d863e6dc1b51666ab
林东吴 <lindongwu11@gmail.com>
2019-08-01 01:14:35 +0800

    :wrench: refactor rescript-service-worker-loader to saparate npm package

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,29 +1,14 @@
-const { appendWebpackPlugin } = require('@rescripts/utilities');
-const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
-
-const logConfig = config => {
-  console.log(config);
-  return config;
-};
-
-function rescriptSWPlugin(serviceWorkerEntryPath) {
-  // const withoutIgnorePlugin = removeWebpackPlugin('SWPrecacheWebpackPlugin', config)
-  return config => {
-    const nextConfig = appendWebpackPlugin(
-      new ServiceWorkerWebpackPlugin({
-        entry: path.join(__dirname, serviceWorkerEntryPath),
-        publicPath: process.env.NODE_ENV === 'development' ? '' : '/ipfs-browser-gateway/'
-      }),
-      config,
-    );
-    return nextConfig;
-  };
-}
 
 module.exports = [
   ['use-babel-config', '.babelrc'],
   ['use-eslint-config', '.eslintrc'],
-  'rescript-disable-eslint',
-  [rescriptSWPlugin, 'src/IPFSServiceWorkerGateway/index.js'],
+  'disable-eslint',
+  [
+    'service-worker-loader',
+    {
+      entry: path.join(__dirname, 'src/IPFSServiceWorkerGateway/index.js'),
+      publicPath: process.env.NODE_ENV === 'development' ? '/' : '/ipfs-browser-gateway/',
+    },
+  ],
 ];
